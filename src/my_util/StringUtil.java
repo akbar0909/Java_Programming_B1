d9fbc22706642c7f6502dc7cee8330ea7535ce5d
Akbar <you@example.com>
2023-04-06 14:18:03 -0400

    day_work practice new method

--- src/my_util/StringUtil.java
+++ src/my_util/StringUtil.java
@@ -11,7 +11,7 @@ public class StringUtil {
     public static String reverseStr (String str) {
 
         String reversed = "";
-        for (int i = str.length()-1; i >= 0; i--) {
+        for (int i = str.length()-1; i >= 1; i--) {
             reversed += str.charAt(i);
         }
         return reversed;
