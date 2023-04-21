e4ee52f6a9bbef9dbfeced6574f283b91c844bc8
Akbar <you@example.com>
2023-04-21 10:36:16 -0400

    day_30 arraylist method

---
+++ src/day31_custom_classes/ThisKeyword.java
@@ -0,0 +1,13 @@
+package day31_custom_classes;
+
+public class ThisKeyword {
+    // this vs this()
+
+    int a = 100;
+
+
+    public ThisKeyword (int a){
+        a = 400;
+        this.a = a;
+    }
+}
