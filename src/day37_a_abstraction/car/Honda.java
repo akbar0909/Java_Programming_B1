59fc427de6e73550445dab288c837b10365374d4
Akbar <you@example.com>
2023-05-03 22:07:31 -0400

    day37 class

---
+++ src/day37_a_abstraction/car/Honda.java
@@ -0,0 +1,11 @@
+package day37_a_abstraction.car;
+
+import day37_a_abstraction.Car;
+
+public class Honda extends Car {
+
+    @Override
+    public void start () {
+        System.out.println("Push the button");
+    }
+}
---
+++ src/day37_a_abstraction/car/Toyota.java
@@ -0,0 +1,14 @@
+package day37_a_abstraction.car;
+
+import day37_a_abstraction.Car;
+
+public class Toyota extends Car {
+
+    @Override
+    public void start(){
+        System.out.println("Insert a key");
+        System.out.println("Start the car by turning the key");
+    }
+
+
+}
