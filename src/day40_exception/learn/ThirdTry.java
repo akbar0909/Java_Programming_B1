8e3b45d800a01cec3214f63a39a1ab6ef14321ad
Akbar <you@example.com>
2023-05-10 22:15:52 -0400

    day40_work

---
+++ src/day40_exception/learn/ThirdTry.java
@@ -0,0 +1,39 @@
+package day40_exception.learn;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ThirdTry {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        try {
+
+            System.out.println("Enter a number ");
+            int num = input.nextInt();
+
+            System.out.println("Enter number 2");
+            int num2 = input.nextInt();
+
+            System.out.println(num / num2);
+
+
+
+        } catch (InputMismatchException e) {
+            e.printStackTrace();
+
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+
+        }finally {
+            System.out.println("FINALLY BLOCK");
+            input.close();
+        }
+
+
+
+
+        System.out.println("Done");
+    }
+}
